586a1a0d07721a854c78c132de582cb1515a69db
Leandro <l.e.c.o321@hotmail.com>
2018-02-05 00:15:40 +0100

    collection_modificated

--- lib/administrate/page/collection.rb
+++ lib/administrate/page/collection.rb
@@ -15,6 +15,18 @@ module Administrate
 
       def attribute_types
         dashboard.attribute_types_for(attribute_names)
+      end      
+      
+      def filter_names
+          dashboard.filter_names
+      end
+
+      def index_actions
+        dashboard.index_actions
+      end
+
+      def index_actions_linea
+        dashboard.index_actions_linea
       end
 
       def ordered_html_class(attr)
